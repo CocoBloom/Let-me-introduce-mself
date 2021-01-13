ae1c06314be2a43c2c44cbb903fe7b3da215de64
CocoBloom <virag.baranyai2@gmail.com>
2021-01-13 14:43:25 +0100

    change a background color

--- framework/database/createUser.sql
+++ framework/database/createUser.sql
@@ -6,9 +6,13 @@ CREATE TABLE user
 
 INSERT INTO user (name, email, password, reg_time)
 VALUES ('Virag', 'virag@gmail.com', '$2y$10$GyP23S3k7TxLMca4io6zcOiAng6Gf/apagyOi.0SyJ4Jh81bQg/5i', NULL);
+# passw: virag
 INSERT INTO user (name, email, password, reg_time)
 VALUES ('Coco', 'coco@gmail.com', '$2y$10$eEunDQy6E6Xma5TdPh2YAuaF7nWJDkKl.DZZIVmdG/IWa.Am/7crS', NULL);
+# passw: coco
 INSERT INTO user (name, email, password, reg_time)
 VALUES ('CodeCool', 'codecool@gmail.com', '$2y$10$W65e614OJ8ZT9VbJXn7OH.5.h/x2TjOx8dlYY6SmT4gdk84mmrc0q', NULL);
+# passw: codecool
 INSERT INTO user (name, email, password, reg_time)
 VALUES ('PHP', 'php@gmail.com', '$2y$10$2lQjM6j5gXrd4luAfTk5lOgzfZWDbLO7HzOp2CbUWK7vXVIFOS5MW', NULL);
+# passw: php
