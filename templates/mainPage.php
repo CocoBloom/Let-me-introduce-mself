e6a48bf0d2d3a2e7bb83515dcd0b6188d96c52ed
CocoBloom <virag.baranyai2@gmail.com>
2021-01-31 13:17:33 +0100

    refactor text

--- templates/mainPage.php
+++ templates/mainPage.php
@@ -12,9 +12,9 @@
             <h3>Üdvözöllek {{ messageContent }}!</h3>
             <p>
                 Baranyai Virág vagyok. 2020. március 2-ikán kezdtem a Full Stack developer kurzust
-                a Codecoolban, előtte közvetlenül Angliában éltem, Bristolban, ahova azért költöztem,
-                mert szerettem volna kipróbálni milyen külföldön élni. 1 évig éltem ott és nagyon megérte.
-                Korábban Budapesten éltem, pincérnőként és pénzügyi munkatársként dolgoztam egy mégdia vállalatnál.
+                a Codecoolban, előtte közvetlenül Angliában éltem, ahova egy évvel korábban költöztem és azért,
+                mert szerettem volna kipróbálni milyen külföldön élni.
+                Korábban Budapesten éltem, pincérnőként és pénzügyi munkatársként dolgoztam egy média vállalatnál.
                 Végzettségem szerint természetvédelmi mérnök vagyok, a diplomát a Nyugat-magyarországi Egyetemen
                 szereztem. A programozást azért kezdtem el, mert szerettem volna hamar egy olyan kézzel fogható
                 szakmát szerezni, ahol meg tudom élni a matematikai, logikus gondolkodás iránti érdeklődésemet.
@@ -23,7 +23,7 @@
                 A Private Info oldal eléréséhez először be kell jelentkezned, utána válik láthatóvá
                 az oldalra vezető gomb formájú link és elérhetővé a route. Az oldalhoz regisztrációs lehetőség nincsen,
                 így az sql file futtatása után az insertált userek valamelyikével tudsz bejelentkezni.
-                Jelszó szintén az sql file-ban. Remélem tetszeni fog az oldal és a megoldásom is. Virág"
+                Jelszó szintén az sql file-ban. Remélem tetszeni fog az oldal és a megoldásom is. Virág
             </p>
         </div>
         <div class="buttonContainer">
