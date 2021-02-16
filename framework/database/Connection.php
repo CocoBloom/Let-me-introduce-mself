babe28ca9abe3c8fbaeaaade0fa56864e3fd92e5
CocoBloom <virag.baranyai2@gmail.com>
2021-02-16 22:43:02 +0100

    change db name

--- framework/database/Connection.php
+++ framework/database/Connection.php
@@ -12,7 +12,7 @@ class Connection
         $host = 'localhost';
         $user = 'Virag';
         $password = 'coco';
-        $dbname = 'introductionToPrioris';
+        $dbname = 'IntroMyself';
         $dsn = "mysql:host=" . $host . ";dbname=" . $dbname;
         $conn = new PDO($dsn, $user, $password);
         $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
